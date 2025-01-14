0ad4f4f7f8f2d014318a0a834f229cc9e5fcbf56
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-01-14 15:55:25 +0000

    changed border colours to be more dark and increased screen size

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import Console from './components/console/console';
 
 export const App: FC = () => {
-  return <Console height={600} width={600} />;
+  return <Console height={1000} width={1000} />;
 };
 
 export default App;
